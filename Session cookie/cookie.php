40d94e3d2a08c597b6b717038a3a0ba5a4cef8fa
先锋Sunny <31482733+LucklyMeme@users.noreply.github.com>
2017-11-20 23:25:37 +0800

    Cookie&Session

---
+++ Session cookie/cookie.php
@@ -0,0 +1,15 @@
+<?php
+setcookie('setByPHP', 'test');
+echo $_COOKIE['setByPHP'];
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
